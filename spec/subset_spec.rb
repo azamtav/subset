1d182e94f50e0e8c5dd369db351f70711d17d57e
Vahid Azam <vahidazam@gmail.com>
2014-06-09 22:03:27 -0500

    updated tests

--- spec/subset_spec.rb
+++ spec/subset_spec.rb
@@ -5,6 +5,9 @@ describe Subset do
 		@sum = 10
 		@set = [1, 3, 5, 7, 9, 11, 15]
 		@subset = Subset.new(@set, @sum)
+		@emptySet = Subset.new([], @sum)
+		@zeroSet = Subset.new(@set, 0)
+		@noMatch = Subset.new(@set, 2000)
 	end
 	
 	describe "#initialize" do
@@ -16,12 +19,29 @@ describe Subset do
 	end
 	
 	describe "#findSubset" do
-		subject (:foundSet) { @subset.findSubset }
-		it "should find a match" do
-			expect(foundSet.set.size).to be > 0
-		end		
-		it "should find a match equal to the sum" do
-			expect(foundSet.sum).to be == @sum
+		context "when it has a match" do			
+			subject (:foundSet) { @subset.findSubset }
+			it "should find a match" do
+				expect(foundSet.set.size).to be > 0
+			end		
+			it "should find a match equal to the sum" do
+				expect(foundSet.sum).to be == @sum
+			end
+		end
+		context "when it has no match" do
+			it "should return nil" do
+				expect(@noMatch.findSubset).to be == nil
+			end		
+		end
+		context "when the sum is zero" do
+			it "should return nil" do
+				expect(@zeroSet.findSubset).to be == nil
+			end
+		end
+		context "when the set is empty" do
+			it "should return nil" do
+				expect(@emptySet.findSubset).to be == nil
+			end
 		end
 	end
 end
